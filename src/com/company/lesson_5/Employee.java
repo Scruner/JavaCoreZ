40a0ed766c12faf580810306ae39ddcce0353c2b
Scruner <aoveden1977@mail.ru>
2020-12-29 01:08:24 +0300

    Initial commit

---
+++ src/com/company/lesson_5/Employee.java
@@ -0,0 +1,33 @@
+package com.company.lesson_5;
+
+public class Employee {
+
+    int id;
+    String surname;
+    int age;
+    double salary;
+    String department;
+
+    Employee(int id, String surname, int age, double salary, String department) {
+        id = id;
+        this.surname = surname;
+        this.age = age;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    void salaryPlus() {
+        System.out.println("Увеличенная зарплата: " + salary * 2);
+    }
+}
+
+class EmployeeTest {
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee(1, "Dick", 5, 23.4, "IT");
+        Employee employee2 = new Employee(2, "Nick", 7, 75.3, "Ximera");
+
+        employee1.salaryPlus();
+        employee2.salaryPlus();
+    }
+}
---
+++ src/com/company/lesson_5/Test20.java
@@ -0,0 +1,18 @@
+package com.company.lesson_5;
+
+public class Test20 {
+
+    int summa(int a, int b, int c) {
+        int result = a + b + c;
+        return result;
+    }
+}
+
+class Test21 {
+    public static void main(String[] args) {
+        Test20 t = new Test20();
+        int summaTrexChisel = t.summa(1, 2, 3);
+        System.out.println(summaTrexChisel);
+        System.out.println(t.summa(5, 12, 7));
+    }
+}
